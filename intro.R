ad6751d00460cd9eab814ded641f1d27821357dd
Kinsey Vo <54725252+kinseyvo@users.noreply.github.com>
2024-02-03 16:10:06 -0800

    iris

---
+++ intro.R
@@ -0,0 +1,157 @@
+9+10
+log(10)
+exp(2)
+help(exp)
+?exp
+a <- 20
+a
+a <- c(10, 20, 30)
+a
+a[1]
+a[2]
+log(a)
+a^2
+a <- a^2
+a
+c(1, 2, 3, 4, 5)
+1:5
+3:7
+?seq
+seq(3:7)
+seq(3, 7)
+a
+a <- c(20, 40, 60)
+a
+a[1]
+a[1:2]
+?mean
+mean(1, 2, 5)
+a <- c(1, 2, 5)
+mean(a)
+a
+a^2
+1+4+25
+sqrt(30)
+a
+b <- a/sqrt(30)
+b
+b
+mean(b)
+b^2
+0.03333333 + 0.13333333 + 0.83333333
+sqrt(1)
+b
+mean(b)
+a
+mean(a)
+mean(b)
+b^2
+b[1]+b[2]+b[3]
+sqrt(1.460593)
+c <- (1:100)
+c
+mean(c)
+course <- list("CPSC", 375, FALSE)
+course[1]
+course
+course <- list(dept="CPSC", number=375, required=FALSE)
+course$dept
+course$number
+course$required
+Course["number"]
+course["number"]
+course[2]
+data.frame(localisation = c("proximal", "distal", "proximal"), tumorsize=c(6.3,8.0,10.0), progress=c(FALSE,TRUE, FALSE))
+df = data.frame(localisation = c("proximal", "distal", "proximal"), tumorsize=c(6.3,8.0,10.0), progress=c(FALSE,TRUE, FALSE))
+df
+df[2,2]
+dr[2,3]
+df[2,]
+dr[2,3]
+df[2,3]
+df[, 3]
+c(10, 20, 30, 40, 45, 56, 74, 23, 200)
+mydata <- c(10, 20, 30, 40, 45, 56, 74, 23, 200)
+mydata
+mydata[3]
+mydata[3:5]
+mydata[c(3, 5)]
+mydata > 50
+mydata[mydata > 50]
+mydata[c(6, 7, 9)]
+which(mydata > 50)
+iris
+view(iris)
+view(iris)
+View(iris)
+str(iris)
+View(iris[10])
+View(iris[1:10])
+View(iris)
+View(iris[:10])
+View(iris[10])
+which(view$10)
+which(iris > 10)
+which(iris$Species == "10")
+which(iris$Species == 10)
+which(iris$Species== "sectosa")
+which(iris$Species== "sectosa")
+which(iris$Species=="sectosa")
+which(iris$Species=="setosa")
+iris[1:50,]
+iris[1:10]
+iris[1:10,]
+class(iris$Sepal.Length)
+View(iris$Sepal.Length)
+View(iris)
+which(iris$Sepal.Length)
+which(iris$Sepal.Length=="TRUE")
+iris$Sepal.Length
+iris[,1]
+iris[10,]
+iris[1:10]
+iris[10]
+iris[10,]
+which(iris$Sepal.Length > 7.6)
+iris[iris$Sepal.Length > 7.6]
+iris[1:10,]
+mean(iris$Sepal.Length)
+which(iris$Sepal.Length > 7.6)
+iris[iris$Sepal.Length > 7.6]
+iris$Sepal.Length > 7.6
+which(iris$Sepal.Length > 7.6)
+iris[iris$Sepal.Length > 7.6, ]
+iris[iris$Sepal.Length > 7.6]
+iris[iris$Sepal.Length > 7.6, ]
+which(iris$Species=="setosa", iris$Sepal.Length>3.0)
+max(iris$Sepal.Length)
+which(max(iris$Sepal.Length))
+which(max(iris$Sepal.Length=7/9))
+which(max(iris$Sepal.Length=7.9))
+which(max(iris$Sepal.Length==7.9))
+which(iris$Sepal.Length==7.9)
+iris[iris$Sepal.Length==7.9]\
+iris[iris$Sepal.Length==7.9]
+iris[iris$Sepal.Length==7.9, ]
+iris[132, "species"]
+iris[132, "Species"]
+iris[4,]
+iris[134, ]
+iris
+View(iris)
+iris[1:50]
+iris[1:50, ]
+iris[c("Sepal.Length")]
+mean(iris$Sepal.Length)
+which(iris$Sepal.Length > 7.6)
+iris(iris$Sepal.Length > 7.6, )
+iris[iris$Sepal.Length > 7.6, ]
+which(iris$Species=="setosa")
+iris[1:50, ]
+which(iris$Sepal.Length>3.0 & iris$Species=="setosa")
+iris[iris$Sepal.Length>3.0 & iris$Species=="setosa", ]
+max(iris$Sepal.Length)
+which(iris$Sepal.Length==7.9)
+iris[132,]
+iris[,132]
+iris[132, "Species"]
